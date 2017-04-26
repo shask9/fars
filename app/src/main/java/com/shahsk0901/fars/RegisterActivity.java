3de8935cb43b7c1ad8044abdb5427e4a265363d3
shahsk0901 <shah.sk0901@gmail.com>
2017-04-26 08:28:32 -0500

    successfully tested firebase connection and value insertion

--- app/src/main/java/com/shahsk0901/fars/RegisterActivity.java
+++ app/src/main/java/com/shahsk0901/fars/RegisterActivity.java
@@ -8,22 +8,32 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private Button register;
-    private TextView textView;
     private EditText netID;
+    private DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        db = FirebaseDatabase.getInstance().getReference("Student");
         register = (Button) findViewById(R.id.register);
+
         View.OnClickListener registerListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 netID = (EditText) findViewById(R.id.netID);
                 if(TextUtils.isEmpty(netID.getText().toString()))
                     netID.setError("Can't be empty");
+
+                Student student = new Student(netID.getText().toString());
+                db.child(netID.getText().toString()).setValue(student);
             }
         };
 
---
+++ app/src/main/java/com/shahsk0901/fars/Student.java
@@ -0,0 +1,9 @@
+package com.shahsk0901.fars;
+
+public class Student {
+    public String netID;
+
+    Student(String netID) {
+        this.netID = netID;
+    }
+}
