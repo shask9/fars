2c9d50390f9eb03e72867bbe664799a0fe7e9d5d
shahsk0901 <shah.sk0901@gmail.com>
2017-04-28 11:20:06 -0500

    Login module complete except admin login

--- app/src/main/java/com/shahsk0901/fars/ForgotPasswordActivity.java
+++ app/src/main/java/com/shahsk0901/fars/ForgotPasswordActivity.java
@@ -3,13 +3,11 @@ package com.shahsk0901.fars;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StudentHome extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_home);
-
-        
+        setContentView(R.layout.activity_forgot_password);
     }
 }
--- app/src/main/java/com/shahsk0901/fars/LoginActivity.java
+++ app/src/main/java/com/shahsk0901/fars/LoginActivity.java
@@ -1,13 +1,97 @@
 package com.shahsk0901.fars;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.*;
+import android.view.*;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private DatabaseReference db;
+    private Student student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText nID = (EditText) findViewById(R.id.netID);
+        final EditText pass = (EditText) findViewById(R.id.password);
+        final TextView register = (TextView) findViewById(R.id.register);
+        final TextView forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        final Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+
+               final String netID = nID.getText().toString();
+               Boolean flag1 = valid(1,netID,nID);
+
+               final String password = pass.getText().toString();
+               Boolean flag2 = valid(2,password,pass);
+
+               if(flag1 && flag2) {
+                   db = FirebaseDatabase.getInstance().getReference("Student/"+netID);
+                   db.addListenerForSingleValueEvent(new ValueEventListener() {
+                       @Override
+                       public void onDataChange(DataSnapshot dataSnapshot) {
+                           if(dataSnapshot.getValue() == null) {
+                               nID.setError("Student not found");
+                           } else {
+                               student = dataSnapshot.getValue(Student.class);
+                               if(student.accountStatus.equals("Suspended")) {
+                                   Toast.makeText(getApplicationContext(),"Account Suspended",Toast.LENGTH_SHORT).show();
+                               } else if(!student.password.equals(password)) {
+                                   Toast.makeText(getApplicationContext(),"Invalid Credentials",Toast.LENGTH_SHORT).show();
+                               } else {
+                                   Intent StudentHome = new Intent(getApplicationContext(),StudentHome.class);
+                                   startActivity(StudentHome);
+                               }
+                           }
+                       }
+
+                       @Override
+                       public void onCancelled(DatabaseError databaseError) {
+
+                       }
+                   });
+               }
+           }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ForgotPasswordActivity = new Intent(getApplicationContext(),ForgotPasswordActivity.class);
+                startActivity(ForgotPasswordActivity);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent RegisterActivity = new Intent(getApplicationContext(), com.shahsk0901.fars.RegisterActivity.class);
+                startActivity(RegisterActivity);
+            }
+        });
+    }
+
+    public Boolean valid(Integer option, String input, EditText editText){
+        Boolean completeData = true;
+        if(option == 1 || option == 2) {
+            if(TextUtils.isEmpty(input)) {
+                editText.setError("Cannot be empty");
+                completeData = false;
+            }
+        }
+        return completeData;
     }
 }
