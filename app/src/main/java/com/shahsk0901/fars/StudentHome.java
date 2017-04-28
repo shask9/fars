e5858b319b7d178a89379e0fa35a241c2355384f
shahsk0901 <shah.sk0901@gmail.com>
2017-04-28 11:31:21 -0500

    implemented logout for student home and a naive way to prevent student from going back to login screen on back press action overriding the backpressed method

--- app/src/main/java/com/shahsk0901/fars/StudentHome.java
+++ app/src/main/java/com/shahsk0901/fars/StudentHome.java
@@ -1,7 +1,10 @@
 package com.shahsk0901.fars;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentHome extends AppCompatActivity {
 
@@ -10,6 +13,19 @@ public class StudentHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
 
-        
+        final Button logout = (Button) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginActivity = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(LoginActivity);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
